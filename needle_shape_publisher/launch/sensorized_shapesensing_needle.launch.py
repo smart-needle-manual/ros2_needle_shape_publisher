1dc0a6c7b02f6323e6b8ebf8a8f90eddabfd83de
Rajdeep Banerjee <20banerjeer@gmail.com>
2026-08-30 21:11:33 -0400

    Simplify launch file by removing unused arguments
    Removed unnecessary launch arguments for number of signals and optimizer iterations.

--- needle_shape_publisher/launch/sensorized_shapesensing_needle.launch.py
+++ needle_shape_publisher/launch/sensorized_shapesensing_needle.launch.py
@@ -10,10 +10,6 @@ def generate_launch_description():
     # arguments
     arg_needleparam = DeclareLaunchArgument( 'needleParamFile',
                                              description="The shape-sensing needle parameter json file." )
-    arg_numsignals = DeclareLaunchArgument( 'numSignals', description="The number of FBG signals to collect.",
-                                            default_value="200" )
-    arg_optim_maxiter = DeclareLaunchArgument( 'optimMaxIterations', default_value="15",
-                                               description="The maximum number of iterations for needle shape optimizer." )
 
     # Nodes
     node_sensorizedneedle = Node(
@@ -24,15 +20,11 @@ def generate_launch_description():
             emulate_tty=True,
             parameters=[ {
                     'needle.paramFile'               : LaunchConfiguration( 'needleParamFile' ),
-                    'needle.numberSignals'           : LaunchConfiguration( 'numSignals' ),
-                    'needle.optimizer.max_iterations': LaunchConfiguration( 'optimMaxIterations' )
                     } ]
             )
 
     # add to launch description
     ld.add_action( arg_needleparam )
-    ld.add_action( arg_numsignals )
-    ld.add_action( arg_optim_maxiter )
 
     ld.add_action( node_sensorizedneedle )
 
